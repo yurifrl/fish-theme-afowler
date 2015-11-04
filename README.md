123f3bd6c48cf8d6198535479922e31d4b81c0f2
Daniel Perez <daniel@claudetech.com>
2015-11-05 07:57:18 +0900

    Initial commit.

---
+++ README.md
@@ -0,0 +1,21 @@
+# fish-theme-afowler
+
+A port of [oh-my-zsh afowler theme](https://github.com/robbyrussell/oh-my-zsh/wiki/themes#afowler) to fish.
+
+It looks like this:
+
+## Installation
+
+## Using [fundle](https://github.com/tuvistavie/fundle)
+
+Add
+
+```
+fundle plugin 'tuvistavie/fish-theme-afowler'
+```
+
+to your fish config, relaunch you shell and run `fundle install`.
+
+## Manual
+
+Source [init.sh](./init.fish) or just copy-paste it in your `config.fish`.
---
+++ init.fish
@@ -0,0 +1,27 @@
+function __theme_afolwer_short_pwd
+	pwd | sed -e 's>'"$HOME"'>~>' | tr / \n | tac | head -n 3 | tac | tr \n / | sed -e 's/\/$//'
+end
+
+function __theme_afowler_git_prompt
+	set -l git_status (git status -z --porcelain 2>&1)
+	if test $status -eq 0
+		set -l raw_ref (command git symbolic-ref HEAD 2> /dev/null; or command git rev-parse --short HEAD 2> /dev/null)
+		set -l ref (echo -n $raw_ref | sed -e 's|refs/heads/||')
+		set -l dirty (test (count $git_status) -gt 0; and echo -n "*")
+		echo -n " $argv[1]$ref$dirty$argv[2]"
+	end
+end
+
+function fish_prompt
+	echo -n (hostname)
+	set_color --bold blue
+	echo -n " :: "
+	set_color normal
+	set_color green
+	echo -n (__theme_afolwer_short_pwd)
+	set_color yellow
+	__theme_afowler_git_prompt "‹" "›"
+	echo
+	set_color --bold blue
+	echo '» '
+end
